84cbdb8384cd703abe03a8f0b09df3cb69c08f68
Qusz <de.gorchakov@gmail.com>
2023-03-13 19:58:27 +0300

    linting

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,21 +1,21 @@
-export default function() {
-  const accordion = document.querySelectorAll('[data-accordion]'),
-        header = document.querySelectorAll('[data-accordion-header]'),
-        body = document.querySelectorAll('[data-accordion-body]');
+export default function () {
+  const accordion = document.querySelectorAll('[data-accordion]');
+  const header = document.querySelectorAll('[data-accordion-header]');
+  const body = document.querySelectorAll('[data-accordion-body]');
 
   const activeClass = 'accordion--active';
 
   header.forEach((item, index) => {
     item.addEventListener('click', () => {
-      let currentParent = accordion[index];
-      let currentBody = body[index];
+      const currentParent = accordion[index];
+      const currentBody = body[index];
 
       currentParent.classList.toggle(activeClass);
 
       if (currentBody.style.maxHeight) {
         currentBody.style.maxHeight = null;
       } else {
-        currentBody.style.maxHeight = currentBody.scrollHeight + 'px';
+        currentBody.style.maxHeight = `${currentBody.scrollHeight}px`;
       }
     });
   });
